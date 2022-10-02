50e3f27499cc89ec6be03022de57c4003a31ae99
Mis Minasyan <misminasyan@icloud.com>
2022-10-03 00:57:04 +0400

    Neaty

---
+++ src/main/java/com/NeatySiteExemple/Neatyy/NeatyyApplication.java
@@ -0,0 +1,13 @@
+package com.NeatySiteExemple.Neatyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeatyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeatyyApplication.class, args);
+	}
+
+}
